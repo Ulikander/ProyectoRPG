f9c8c2e1d86384284320317d18e8d5bc9f692dbf
Winnkz <winnkz_13@github.com>
2018-12-02 13:58:39 -0600

    Desplazamiento en mapa
    New features:
    - Escena de prueba para cambio de seccion del mapa
    - Cambio de camara entre zonas
    - Camara sigue al jugador en zona de boss

---
+++ ProyectoRPG/Assets/Scripts/MapMovement/BossCameraMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossCameraMovement : MonoBehaviour {
+
+    public GameObject childMap;
+    public GameObject camera;
+
+    void Start()
+    {
+        childMap = this.gameObject.transform.GetChild(0).gameObject;
+        camera = GameObject.Find("Main Camera");
+        childMap.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+        {
+            
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+        {
+            Debug.Log("Salio");
+            childMap.SetActive(false);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+        {
+            Debug.Log("Dentro del mapa");
+            childMap.SetActive(true);
+            camera.transform.position = new Vector3(collision.gameObject.transform.position.x, collision.gameObject.transform.position.y, camera.transform.position.z);
+        }
+    }
+}
