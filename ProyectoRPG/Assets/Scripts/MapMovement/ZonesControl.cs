84d55c57fe474098a45008ecbaed6546e43c0c8d
Ulises Martínez Sandoval <36496692+Ulikander@users.noreply.github.com>
2018-12-10 00:10:43 -0600

    Update_Camara+NPC
    Scripts:
    Attack (Ulises): Es el script que usan los ataques, esta planeado para ser usado por los Enemigos, el Player y hasta los npc. Este controlará la animación y causará daño a un objetivo con un colisionador dependiendo de quien lo casteó.
    
    NPC (Ulises y Benja): Se creó el código de movimiento del npc mediante waypoints. Hereda de Movement y pronto se agregará el sistema de conversación.
    
    ZoneMovement, BossCameraMovement y MapMovement (Emanuel): Controla el scroll de la cámara y también cuando se mueve, se adaptará la escena para que haga desaparecer tilesets de determinados mapas.
    
    EnemyAI y Player (Ulises): Pequeños Tweaks, la mayor parte de los cambios es en Player para interactuar con el Karma (Reputación).
    
    LogicaDifusa (Benja): Este script manejará el calculo del Karma del personaje (Falta agregar)
    
    -------------------
    Se agregaron algunas animaciones y se hizo slice a un NPC.

---
+++ ProyectoRPG/Assets/Scripts/MapMovement/ZonesControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZonesControl : MonoBehaviour {
+
+    public GameObject childMap;
+    public GameObject camera;
+    public Vector3 temp;
+    public static bool flagControl = false;
+    public static bool flagTemp;
+
+    public virtual void Start () {
+        childMap = gameObject.transform.GetChild(0).gameObject;
+        camera = GameObject.Find("Main Camera");
+        childMap.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    /*
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+    */
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+        {
+            Debug.Log("Salio");
+            childMap.SetActive(false);
+            
+        }
+    }
+    /*
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+
+    }
+    */
+}
